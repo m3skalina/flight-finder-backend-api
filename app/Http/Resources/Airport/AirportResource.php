c7ac88cd5266f1970158a87fbc3bfc178965a039
Fabio <m3skalina@gmail.com>
2023-08-10 18:22:31 +0200

    created api resources: - FlightResource - AirportResource

---
+++ app/Http/Resources/Airport/AirportResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Airport;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AirportResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'code' => $this->code,
+            'name' => $this->name,
+            'longitude' => $this->long,
+            'latitude' => $this->lat,
+        ];
+    }
+}
---
+++ app/Http/Resources/Flight/FlightResource.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Resources\Flight;
+
+use App\Http\Resources\Airport\AirportResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FlightResource extends JsonResource
+{
+
+    protected $departure_airport;
+    protected $arrival_airport;
+
+    public function setDepartureAirport($departure_airport)
+    {
+        $this->departure_airport = $departure_airport;
+        return $this;
+    }
+
+    public function setArrivalAirport($arrival_airport)
+    {
+        $this->arrival_airport = $arrival_airport;
+        return $this;
+    }
+
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @mixin Flight
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'departure' => AirportResource::make($this->departure_airport),
+            'arrival' => AirportResource::make($this->arrival_airport),
+            'price' => $this->price,
+        ];
+    }
+}
